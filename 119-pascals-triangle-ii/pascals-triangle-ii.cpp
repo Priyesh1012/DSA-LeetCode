39971897da847dd14e534c82cf83347aafe6522b
Priyesh Nandanwar <89803155+Priyesh1012@users.noreply.github.com>
2026-06-16 19:55:24 +0530

    Time: 2 ms (9.44%) | Memory: 9.5 MB (6.20%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex){
+
+        int n = rowIndex+1; 
+
+        vector<vector<int>> output(n);
+        if(n>0){
+            output[0] = {1};
+        }
+        if(n>1){
+            output[1] = {1,1};
+        }
+
+        for(int i = 2;i<n;i++){
+            for(int j = 0; j<=i ;j++){
+                   if(j==0 or j==i){
+                        output[i].push_back(output[i-1][0]);
+                    }
+                    else{
+                        output[i].push_back(output[i-1][j-1]+output[i-1][j]);
+                    }
+            }
+
+        }     
+
+        return output[rowIndex]; 
+    }
+      
+    
+};
