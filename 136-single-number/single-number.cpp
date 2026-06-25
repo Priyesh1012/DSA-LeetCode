156514d436397d8b680189af6b516b556914a05e
Priyesh Nandanwar <89803155+Priyesh1012@users.noreply.github.com>
2026-06-26 02:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (23.81%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int key = nums[0];
-        bool ret = true;
-        for(int i = 0;i<nums.size();i++){
-            ret = true;
-            for(int j = 0;j<nums.size();j++){
-                if(key == nums[j] && j!=i){
-                    ret = false;
-                    key = nums[i+1];
-                }
-            }
-            if(ret){
-                return nums[i];
-            }
-        } 
-        return(nums[0]);
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
     }
 };
